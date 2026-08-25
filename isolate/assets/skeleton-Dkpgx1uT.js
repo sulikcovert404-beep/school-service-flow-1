563f1d39f6ebc2b2ac8fb2295e2e5f9a8b2a25b0
fghg fghfh <320860096+sulikcovert404-beep@users.noreply.github.com>
2026-08-25 07:45:48 +0000

    Before: خوب الان چه مرحله ای از پروژه هستیم

---
+++ isolate/assets/skeleton-Dkpgx1uT.js
@@ -0,0 +1,1 @@
+import{j as a}from"./framer-motion-Cb6p-da6.js";import{a as o}from"./index-C5wAV0hN.js";function r({className:t,...e}){return a.jsx("div",{"data-slot":"skeleton",className:o("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
